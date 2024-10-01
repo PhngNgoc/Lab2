82afd1c7f0326cbc6fad0f407bfcabbd4edf640e
PhngNgoc <159431247+PhngNgoc@users.noreply.github.com>
2024-10-01 14:50:05 +0700

    Update main.c

--- stm32/Core/Src/main.c
+++ stm32/Core/Src/main.c
@@ -96,10 +96,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-<<<<<<< Updated upstream
-=======
 	  ex1();
->>>>>>> Stashed changes
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
